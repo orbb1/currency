fe829ee1558dc54dab2b6696dd7e0bc4d83ebba3
orbb <orbb@localhost.localdomain>
2017-02-11 17:52:58 +0100

    dashboard component displays response data

--- client/components/dashboard/dashboard.js
+++ client/components/dashboard/dashboard.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
+import dashboardComponent from './dashboard.component';
 
-let dashboardModule = angular.module('dashboard', []).name;
+let dashboardModule = angular.module('dashboard', [])
+.component('dashboard', dashboardComponent)
+.name;
 
 export default dashboardModule;
--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -3,8 +3,10 @@ import uiRouter from 'angular-ui-router';
 import homeComponent from './home.component';
 import homeConfig from './home.config';
 import SpaceService from '../../services/space.service';
+import dashboardModule from '../../components/dashboard/dashboard';
 
 let homeModule = angular.module('pages.home', [
+    dashboardModule,
     uiRouter
 ]).component('home', homeComponent)
 .service('SpaceService', SpaceService)
